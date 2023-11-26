b874de5f3d8a0f0876122070772e6840be7db3c2
anup199 <anupsingh6625@gmail.com>
2023-11-26 22:37:55 +0530

    adding project

---
+++ app.js
@@ -0,0 +1,33 @@
+const endDate = "21 October 2023 11:58 PM";
+
+document.getElementById("end-date").innerText=endDate;
+
+const inputs = document.querySelectorAll("input");
+
+// const clock=(){
+
+// }
+function clock()
+{
+     const end = new Date(endDate);
+     const now = new Date();
+
+     const diff = (end-now)/1000;  //convert into seconds
+
+     if(diff<0) return;
+
+     // console.log(diff);
+     inputs[0].value = Math.floor(diff/3600/24);
+     inputs[1].value = Math.floor(diff/3600)%60;
+     inputs[2].value = Math.floor(diff/60)%60;
+     inputs[3].value = Math.floor(diff)%60;
+}
+
+
+clock();
+
+setInterval(
+     () =>{
+          clock()
+     },1000
+)
